a77cea9523e0b00e485dd72cc2587fdbeeaf1704
Merav-M <merav.mauhnoom@post.runi.ac.il>
2024-11-20 23:54:06 +0200

    Cheers submition

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,36 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+        String sWord = args[0];
+        String sUpperCasedWord = "";
+
+        int nWordLength = sWord.length();
+
+        for (int i = 0; i < nWordLength; i++) {
+                char nCh = sWord.charAt(i);
+                if (nCh >= 'a' && nCh <= 'z') {
+                    nCh = (char) (nCh - 'a' + 'A');
+                }
+                sUpperCasedWord += nCh;
         }
+    
+        
+        for ( int i = 0 ; i <nWordLength ; ++i) {
+
+                String cArticle = "an";
+                char cCurrChar = sUpperCasedWord.charAt(i);
+                if ("AEFHILMNORSX".indexOf(cCurrChar) == -1) {
+                        cArticle = "a";
+                }
+                System.out.println("Give me " + cArticle + ":" + sUpperCasedWord.charAt(i));
+        }
+
+        System.out.println("What does that spell?");
+
+        int nNumOfTimes = Integer.parseInt(args[1]);
+
+        for (int j = 0; j<nNumOfTimes; ++j) {
+                System.out.println(sUpperCasedWord + " !!!");
+        }
+}
 }
